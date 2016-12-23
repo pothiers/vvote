8462b525d8e31a21ecec25a1bf458237337d8ddf
Steve Pothier <pothiers@gmail.com>
2016-12-23 10:32:40 -0700

    count of specialized tags (overvote, etc) done on each raceballot

--- vvote/vvote.py
+++ vvote/vvote.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-"""<<Python script callable from command line.  Put description here.>>
+"""Count ballots.
 """
 
 import sys
@@ -11,95 +11,121 @@ from openpyxl import load_workbook
 
 def OLD1_emit_results(votes, choices):
     #print('votes={}'.format(votes))
-    # votes[issue][choice] = count
+    # votes[race][choice] = count
     print("Ballot Counts:")
-    for issue in sorted(choices.keys()):
-        print('  Issue: "{}"'.format(issue))
-        for choice in sorted(choices[issue]):
-            print('    "{}" = {}'.format(choice,votes[issue][choice]))
+    for race in sorted(choices.keys()):
+        print('  Race: "{}"'.format(race))
+        for choice in sorted(choices[race]):
+            print('    "{}" = {}'.format(choice,votes[race][choice]))
 
 def emit_results(votes, choices, n_votes,
-                 orderedissues=None,
-                 na_choice=None, file=sys.stdout):
-    # votes[issue][choice] = count
-    # choices[issue] = set([choice1, choice2,...])
-    # n_votes[issue] => num-to-vote-for
+                 orderedraces=None,
+                 na_tag=None, file=sys.stdout):
+    # votes[race][choice] = count
+    # choices[race] = set([choice1, choice2,...])
+    # n_votes[race] => num-to-vote-for
 
-    # Output the number of votes allowed for each issue (to stdout)
-    print('Vote for N:')
-    for k,v in n_votes.items():
-        print('{}:\t{}'.format(v,k))
+    # Output the number of votes allowed for each race (to stdout)
+    #! print('Vote for N:')
+    #! for k,v in n_votes.items():
+    #!     print('{}:\t{}'.format(v,k))
 
-    for issue in orderedissues:
+    for race in orderedraces:
         print(file=file)
-        print(issue, file=file)
-        for choice in choices[issue]:
-            if choice == na_choice:
-                n = n_votes[issue]
-                print('\t{}\t{}'.format(choice, int(votes[issue][choice]/n)),
+        print(race, file=file)
+        for choice in choices[race]:
+            if choice == na_tag:
+                n = n_votes[race]
+                print('\t{}\t{}'.format(choice, int(votes[race][choice]/n)),
                       file=file)
             else:
-                print('\t{}\t{}'.format(choice, votes[issue][choice]),
+                print('\t{}\t{}'.format(choice, votes[race][choice]),
                       file=file)
 
 def count_votes(xslx_filename,
                 outputfile=None,
-                na_choice=' Out of District Ballots'):
+                na_tag='<OOD>', # Out of District Ballots
+                writeintag='Write-in',
+                overvotetag='overvote',
+                undervotetag='undervote'):
     wb = load_workbook(filename=xslx_filename, read_only=True)
     ws = wb.active
     nontitles = set(['Cast Vote Record',
                      'Serial Number',
                      'Precinct',
                      'Ballot Style'])
-    choices = defaultdict(set) # choices[issue] = set([choice1, choice2,...])
-    votes = defaultdict(lambda : defaultdict(int)) # votes[issue][choice] = cnt
-    coltitle = dict() # coltitle[column] => issuetitle
-    orderedissues = list()
+    choices = defaultdict(set) # choices[race] = set([choice1, choice2,...])
+    votes = defaultdict(lambda : defaultdict(int)) # votes[race][choice] = cnt
+    n_votes = defaultdict(int)  # n_votes[race] => num-to-vote-for
+
+    coltitle = dict() # coltitle[column] => racetitle
+    orderedraces = list()
     prevtitle = None
     ridx = 0
-    need_nvotes = True
     for row in ws.rows:
         ridx += 1
         cidx = 0
+        raceballot = list() # single ballot for single race
         for cell in row:
             cidx += 1
 
-            # Ignore the (leading) columns that are not Issue Titles
+            # Ignore the (leading) columns that are not Race Titles
             if ridx == 1 and cell.value in nontitles:
                 continue
             
-            if ridx == 1:
-                if cell.value == None:
-                    title = prevtitle
-                else:
-                    title = cell.value
-                    orderedissues.append(title)
-
-                prevtitle = title
-                coltitle[cidx] = title
-            else:
-                if need_nvotes:
-                    # n_votes[issue] => num-to-vote-for
-                    n_votes = defaultdict(int) 
-                    for k,v in coltitle.items():
-                        n_votes[v] += 1
-                    need_nvotes = False
+            if ridx == 1: # header
+                if cell.value != None:
+                    race = cell.value
+                    orderedraces.append(race)
+                    n_votes[race] = 1
+                    choices[race].add(undervotetag)
+                    choices[race].add(overvotetag)
+                    choices[race].add(na_tag)
+                else: # vote-for-N race
+                    n_votes[race] += 1
+                coltitle[cidx] = race
+            else: # ballots
+                if cidx not in coltitle: # should never happen
+                    continue
                 
                 if (cell.value == '' or cell.value == None):
-                    value = na_choice
+                    choice = na_tag
                 else:
-                    value = cell.value
-
-                if cidx in coltitle:
-                    issue = coltitle[cidx]
-                    choice = value
-                    choices[issue].add(choice)
-                    votes[issue][choice] += 1
+                    choice = cell.value
+                race = coltitle[cidx]
+                next_race = coltitle[cidx+1] if cidx < ws.max_column else None
+                raceballot.append(choice)
+                if race != next_race: # finished one race, one ballot
+                    #print('DBG-1: raceballot={}'.format(raceballot))
+                    if undervotetag in raceballot:
+                        undervote_m = ('undervote-{}'
+                                       .format(raceballot.count(undervotetag)))
+                        votes[race][undervote_m] += 1
+                    if overvotetag in raceballot:
+                        assert raceballot.count(overvotetag) == n_votes[race]
+                        votes[race][undervotetag] += 1
+                    if na_tag in raceballot:
+                        assert raceballot.count(na_tag) == n_votes[race]
+                        votes[race][na_tag] += 1
+                    raceballot = [c for c in raceballot
+                                  if ((c != undervotetag)
+                                      and (c != overvotetag)
+                                      and (c != na_tag)
+                                      and (c != writeintag)
+                                  )]
+                    #! if len(raceballot) != len(set(raceballot)):
+                    #!     print('raceballot[{}]={}'.format(ridx,raceballot))
+                    assert len(raceballot) == len(set(raceballot)) # no dupes
+                    for choice in set(raceballot):
+                        choices[race].add(choice)
+                        votes[race][choice] += 1
+                    raceballot = list() # single ballot for single race
 
     # Vote counts now in: votes
-    emit_results(votes, choices, n_votes, na_choice=na_choice,
-                 orderedissues=orderedissues,
+    emit_results(votes, choices, n_votes, na_tag=na_tag,
+                 orderedraces=orderedraces,
                  file=outputfile)
+    print('Processed {} ballots'.format(ridx-1))
     return votes,choices
 
 
