6b73a6c2bd86f82a87275ccee25d8a812d4a9df3
Steve Pothier <pothiers@gmail.com>
2017-01-31 19:19:34 -0700

    snap

---
+++ docs/sphinx/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:51,filenames:["index.rst"],objects:{"":{vvote:[0,0,0,"-"]}},objnames:{"0":["py","module","Python module"]},objtypes:{"0":"py:module"},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to VVOTE&#8217;s documentation!"],titleterms:{document:0,indic:0,tabl:0,vvote:0,welcom:0}})
