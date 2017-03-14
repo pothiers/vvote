49bb8d83bd7700823fd2a962733ccd2bb1763051
Steve Pothier <pothier@noao.edu>
2017-03-14 06:29:09 -0700

    snap

--- examples.sh
+++ examples.sh
@@ -14,11 +14,12 @@ cd ~/sandbox/vvote
 # Create mappings for RACE and CHOICE titles (SOVC to LVR)
 genmap -v $data/day1-sovc.xlsx $data/day1-lvr.xlsx rm.csv cm.csv
 
+countvote -v --format text $data/day1-lvr.xlsx day1.txt
 
 # Tally votes. Output as SOVC format. The SOVC will be the calculated
 # tallys that we can compare to the official SOVC except that the RACE
 # and CHOICE strings may differ somewhat.
-countvote --format SOVC $data/2016GenSampleSet-400.cvr.xlsx 2016GenSampleSet-400.sovc.xlsx 
+countvote --format SOVC $data/2016GenSampleSet-400.lvr.xlsx 2016GenSampleSet-400.sovc.xlsx 
 
 # UNDER-CONSTRUCTION!!!
 comparesovc --racemap rm.csv --choicemap cm.csv 2016GenSampleSet-400.sovc.xlsx $data/2016GenSampleSet-400.sovc.xlsx
