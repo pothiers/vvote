67dbd0c105b2d350d4610a279bb6f7373b78ea35
Steve Pothier <pothiers@gmail.com>
2018-01-03 17:07:50 -0700

    snap

---
+++ install_from_clone.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# After git clone (or pull), run this to install CLI.
+#
+# Requirements:
+#   python3
+#
+SCRIPT=$(readlink -f $0)        # Absolute path to this script
+SCRIPTPATH=$(dirname $SCRIPT)   # Absolute path this script is in
+
+installprefix=$SCRIPT/venv
+repodir=${1:-$SCRIPT}
+
+pushd $repodir > /dev/null
+
+python3 -m venv --without-pip venv
+source venv/bin/activate
+curl https://bootstrap.pypa.io/get-pip.py | python
+deactivate
+source venv/bin/activate
+
+pip install -r $repodir/requirements.txt
+pylint -E vvote/
+pylintstatus=$?
+if [ $pylintstatus -eq 1 ]; then
+    echo "pylint FATAL message for VVOTE"
+    exit 1
+fi
+if [ $pylintstatus -eq 2 ]; then
+    echo "pylint ERROR message for VVOTE"
+    exit 2
+fi
+if [ $pylintstatus -gt 0 ]; then
+    echo "pylint returned non-zero status for VVOTE"
+    exit 2
+fi
+
+python3 setup.py install --force --prefix $installprefix
